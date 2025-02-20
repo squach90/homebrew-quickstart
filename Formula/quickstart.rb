28042ae100c42d5b7edee7a81ee71563341d5cfa
squach90 <160175686+squach90@users.noreply.github.com>
2025-02-20 12:32:41 +0100

    Mise à jour vers v1.1.0

--- Formula/quickstart.rb
+++ Formula/quickstart.rb
@@ -1,9 +1,9 @@
 class Quickstart < Formula
   desc "Un CLI pour créer rapidement des projets HTML, Python, Node, etc."
   homepage "https://github.com/squach90/homebrew-quickstart"
-  url "https://github.com/squach90/homebrew-quickstart/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "38c446f9a85aebaa44a8575aef623666b4b37f489a90c017ee13141563742964"
-  version "1.0.0"
+  url "https://github.com/squach90/homebrew-quickstart/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "21749c869375878901ef9b360121a6179217df5aaf66c32213b99e1654892f8e"
+  version "1.1.0"
 
   def install
     bin.install "QuickStart.sh" => "quickstart"
