73fd94b0ad89daf9671844ab3cf367cb12fbb51f
squach90 <160175686+squach90@users.noreply.github.com>
2025-02-20 21:37:32 +0100

    fix file path

--- Formula/quickstart.rb
+++ Formula/quickstart.rb
@@ -1,7 +1,7 @@
 class Quickstart < Formula
   desc "A CLI to quickly create HTML, Python, Node, etc. projects."
   homepage "https://github.com/squach90/homebrew-quickstart"
-  url "https://github.com/squach90/homebrew-quickstart/releases/tag/v1.4.4"
+  url "https://github.com/squach90/homebrew-quickstart/archive/refs/tags/v1.4.4.tar.gz"
   sha256 "d37d736eb8523caddc091883e3bc9acb33e72e1ca5abf55fce8d79c10b52feb5"
   version "1.4.4"
 
