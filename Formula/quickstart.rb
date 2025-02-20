bd4dd37019ef10bfa2164b7f61c721f1e35f1d15
squach90 <160175686+squach90@users.noreply.github.com>
2025-02-20 21:16:46 +0100

    test

--- Formula/quickstart.rb
+++ Formula/quickstart.rb
@@ -2,7 +2,7 @@ class Quickstart < Formula
   desc "A CLI to quickly create HTML, Python, Node, etc. projects."
   homepage "https://github.com/squach90/homebrew-quickstart"
   url "https://github.com/squach90/homebrew-quickstart/releases/tag/v1.4.2"
-  sha256 "59244f4cdce87f64586330d16c41821e2c8d4cbb6e81df549be321f78e38da61"
+  sha256 "5b379ec04734edb75717c4797abec36fc6b2355f5c7f6270bd8b5c303e4027dd"
   version "1.4.2"
 
   def install
