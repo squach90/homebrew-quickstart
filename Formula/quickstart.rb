4d1dfca7451abf0fb2adc233738d7ca44aca49f7
squach90 <160175686+squach90@users.noreply.github.com>
2025-02-20 12:16:42 +0100

    change details

--- Formula/quickstart.rb
+++ Formula/quickstart.rb
@@ -1,8 +1,8 @@
-class Projinit < Formula
+class QuickStart < Formula
   desc "Un CLI pour créer rapidement des projets HTML, Python, Node, etc."
   homepage "https://github.com/squach90/homebrew-quickstart"
   url "https://github.com/squach90/homebrew-quickstart/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "TON_SHA256"
+  sha256 "38c446f9a85aebaa44a8575aef623666b4b37f489a90c017ee13141563742964"
   version "1.0.0"
 
   def install
