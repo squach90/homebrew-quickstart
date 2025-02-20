ec540bc1503f7e794888da8293bd160196818533
squach90 <160175686+squach90@users.noreply.github.com>
2025-02-20 19:17:52 +0100

    Mise à jour vers v1.3.0, Installation des dépendences nécessaire si pas présente

--- Formula/quickstart.rb
+++ Formula/quickstart.rb
@@ -1,9 +1,9 @@
 class Quickstart < Formula
   desc "A CLI to quickly create HTML, Python, Node, etc. projects."
   homepage "https://github.com/squach90/homebrew-quickstart"
-  url "https://github.com/squach90/homebrew-quickstart/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "86575024265839d8686c1344554aecb8f883236259df55ccf04ff0f1029e6ef4"
-  version "1.2.0"
+  url "https://github.com/squach90/homebrew-quickstart/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "c7ebaa8576023ef377ffed9bcf458998fc9a55a3fa1255adbfaad94ed8187f1a"
+  version "1.3.0"
 
   def install
     bin.install "QuickStart.sh" => "quickstart"
