837dd8809d05bc9103b6f7f6392b5ba7eeb2c272
squach90 <160175686+squach90@users.noreply.github.com>
2025-02-21 15:45:55 +0100

    Switch to v1.5.2

--- Formula/quickstart.rb
+++ Formula/quickstart.rb
@@ -1,9 +1,9 @@
 class Quickstart < Formula
   desc "A CLI to quickly create HTML, Python, Node, etc. projects."
   homepage "https://github.com/squach90/homebrew-quickstart"
-  url "https://github.com/squach90/homebrew-quickstart/archive/refs/tags/v1.5.1.tar.gz"
-  sha256 "e65f2c76fb5a5090f6cc964dc4140d189800cd7afbdfecbd01fb89343ec4648c"
-  version "1.5.1"
+  url "https://github.com/squach90/homebrew-quickstart/archive/refs/tags/v1.5.2.tar.gz"
+  sha256 "79587d6183e8cbb5167fb39268079dc00804806a69a9cf9f5093524261918b6a"
+  version "1.5.2"
 
   depends_on "gum"
 
