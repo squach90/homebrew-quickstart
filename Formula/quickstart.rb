7deffc99d61fad725162bede3164949343be15a5
squach90 <160175686+squach90@users.noreply.github.com>
2025-02-23 16:44:05 +0100

    Switch to v1.5.3

--- Formula/quickstart.rb
+++ Formula/quickstart.rb
@@ -1,9 +1,9 @@
 class Quickstart < Formula
   desc "A CLI to quickly create HTML, Python, Node, etc. projects."
   homepage "https://github.com/squach90/homebrew-quickstart"
-  url "https://github.com/squach90/homebrew-quickstart/archive/refs/tags/v1.5.2.tar.gz"
-  sha256 "79587d6183e8cbb5167fb39268079dc00804806a69a9cf9f5093524261918b6a"
-  version "1.5.2"
+  url "https://github.com/squach90/homebrew-quickstart/archive/refs/tags/v1.5.3.tar.gz"
+  sha256 "d41a8be659575dc019afab8e1d0d775739812af9c8654f9e4e051cfb3fe9dd1e"
+  version "1.5.3"
 
   depends_on "gum"
 
--- Formula/test/quickstart.rb
+++ Formula/test/quickstart.rb
@@ -2,7 +2,7 @@ require "formula_test_helper"
 
 class QuickstartTest < FormulaTest
   def test_version
-    assert_equal "1.5.2", shell_output("#{bin}/quickstart --version").chomp
+    assert_equal "1.5.3", shell_output("#{bin}/quickstart --version").chomp
   end
 
   def test_basic_functionality
