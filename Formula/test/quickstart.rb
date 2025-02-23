3d215de1ab13390497378f6a8bc0a6223cc89997
squach90 <160175686+squach90@users.noreply.github.com>
2025-02-23 16:42:40 +0100

    Add Test

---
+++ Formula/test/quickstart.rb
@@ -0,0 +1,12 @@
+require "formula_test_helper"
+
+class QuickstartTest < FormulaTest
+  def test_version
+    assert_equal "1.5.2", shell_output("#{bin}/quickstart --version").chomp
+  end
+
+  def test_basic_functionality
+    system "#{bin}/quickstart", "--help"
+  end
+end
+
